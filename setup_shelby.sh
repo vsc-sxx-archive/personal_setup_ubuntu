462c5618f92f08fbafb6b83e64ded15fb434ba6d
Shelby <shelbyhell@duck.com>
2022-11-17 07:49:41 +0400

    Update setup_shelby.sh

--- setup_shelby.sh
+++ setup_shelby.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
-
-install_pkg() {
 echo "Start install packages for building"
 sudo apt-get update -q -y && sudo apt-get install -q -y --no-install-recommends \
     apt-utils apt-transport-https python3-apt \
@@ -19,15 +17,11 @@ sudo apt-get update -q -y && sudo apt-get install -q -y --no-install-recommends 
     ncurses-bin libncurses5-dev lib32ncurses5-dev bc libreadline-gplv2-dev libsdl1.2-dev libtinfo5 python-is-python2 ninja-build libcrypt-dev\
     file gawk xterm screen rename tree schedtool software-properties-common \
     dos2unix jq flex bison gperf exfat-utils exfat-fuse libb2-dev pngcrush imagemagick optipng advancecomp kmod gem ruby tmate
-}
 
-github() {
+apt install binutils-arm-none-eabi gcc-arm-none-eabi libstdc++-arm-none-eabi-newlib binutils-aarch64-linux-gnu gcc-aarch64-linux-gnu -y
+
 echo "Configure github."
 sudo apt update && sudo apt install git -y
 git config --global user.name 'Alexey Shelby (shelbyhell.)'
 git config --global user.email "shelbyhell@proton.me"
 git config --global credential.helper "cache --timeout=7200"
-}
-
-install_pkg
-github
